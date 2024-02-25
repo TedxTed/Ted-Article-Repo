c2c513061ccdf4e916fd5527bfbc372215c2b9df
Ted <wodf410@gmail.com>
2024-02-25 23:36:45 +0800

    wip 20240225 2356

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -41,6 +41,28 @@ const Comments = () => {
             consectetur auctor ullamcorper. Proin cursus eu purus ut fringilla.
           </p>
         </div>
+
+        <div className={styles.comment}>
+          <div className={styles.user}>
+            <Image
+              src="/p1.jpeg"
+              alt=""
+              width={50}
+              height={50}
+              className={styles.image}
+            />
+            <div className={styles.userInfo}>
+              <span className={styles.username}>John Doe</span>
+              <span className={styles.date}>10.23.2024</span>
+            </div>
+          </div>
+          <p className={styles.desc}>
+            Maecenas iaculis dui nunc, quis tempus lectus dignissim et.
+            Curabitur sed vulputate erat. Integer congue luctus imperdiet.
+            Maecenas aliquam mi pellentesque aliquam lobortis. Nullam
+            consectetur auctor ullamcorper. Proin cursus eu purus ut fringilla.
+          </p>
+        </div>
       </div>
     </div>
   );
