9b01418ca4f265146059de1723781655d8f326d5
Ted <wodf410@gmail.com>
2024-02-25 11:31:04 +0800

    wip flex

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,10 @@
 import styles from "./homepage.module.css";
 import Featured from "@/components/featured/Featured";
 import CategoryList from "@/components/categoryList/CategoryList";
-import CardList from "@/components/card/Card";
-import Menu from "@/components/menu/Menu";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
-export default function Home() {
+export default function Home({ searchParams }) {
   return (
     <div className={styles.container}>
       <Featured />
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import styles from "./card.module.css";
-import Pagination from "../pagination/Pagination";
+import Image from "next/image";
+import Link from "next/link";
 
 const Card = () => {
   return (
     <div className={styles.container}>
-      <Pagination />
+      <div className={styles.imageContainer}>
+        <Image src="/p1.jpeg" alt="" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>11.02.2024 - </span>
+          <span className={styles.category}>CUTURE</span>
+        </div>
+        <Link href="/">
+          <h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+          </h1>
+        </Link>
+        <p className={styles.desc}>
+          orem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum
+        </p>
+        <Link href="/" className={styles.link}>
+          Read More
+        </Link>
+      </div>
     </div>
   );
 };
